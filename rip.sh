7862964ab08910cdfaf2bba2924ff334e05cda13
RIP <78027212+urfavrip@users.noreply.github.com>
2023-01-29 07:20:18 +0800

    Add files via upload

--- rip.sh
+++ rip.sh
@@ -7,10 +7,4 @@ apt-get install wget
 apt-get install proot
 apt-get install cmake
 
-git clone https://github.com/xmrig/xmrig
-cd rip
-mkdir build
-cd build
-cmake -DWITH_HWLOCK=OFF
-
-echo "All dependencies have been installed, please run the command \"make\" to immediately start the script"
+echo "All dependencies have been installed, please run the command \"run script mining\" to immediately start the script"
